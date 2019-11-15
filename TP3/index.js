9de2dce54c0a6ae2af7d0a99f357276cdcc8ee6d
abdelkader <abdelkader.zobiri@polymtl.ca>
2019-11-15 14:08:23 -0500

    loloKaty5

--- TP3/index.js
+++ TP3/index.js
@@ -1,24 +1,27 @@
+var barChart;
+var horizontalBarChart_A, horizontalBarChart_B ;
 var inputList = ["Price Shift Alpha", "High Data Tariff (Alpha only)", "Price Shift Beta", "Price Shift Gamma"];
+var competitorDataList = [100,80,30,50];
+var products = ["Product 1", "Product 2", "Product 3", "Product 4"];
 // these two functions allows us to show or hide the images depending on wether we changed the inout or not
 function loadPage() {
+	createGraph();
 	var input = document.getElementsByTagName('select');
-
 	for (i = 0; i < input.length; i++) {
-		input[i].addEventListener('change', () => (UpdateGraph())); // addEventListener used
+		input[i].addEventListener('change', () => (updateGraph())); // addEventListener used
 	}
-	UpdateGraph();
 }
 
 
 // Aternating between the image and the json output
 
-function UpdateGraph() {
+function createGraph() {
 		
 	// Bar chart
-	chart = new Chart(document.getElementById("bar-graph"), {
+	barChart = new Chart(document.getElementById("bar-graph"), {
 		type: 'bar',
 		data: {
-			labels: ["Product 1", "Product 2", "Product 3", "Product 4"],
+			labels: [],
 			datasets: [
 			{
 				label: "Company A",
@@ -27,7 +30,7 @@ function UpdateGraph() {
 			}, {
 				label: "Company B",
 				backgroundColor: "#8e5ea2",
-				data: [408,547,675,734]
+				data: []
 			}
 			]
 		},
@@ -35,15 +38,95 @@ function UpdateGraph() {
 			title: {
 			display: true,
 			text: 'Price ($CAD)'
+			},
+			scales: {
+				yAxes: [{
+					ticks: {
+							beginAtZero: true
+						}
+				}]
 			}
+
 		}
 	});
-	for ( i=0; i< inputList.length; i++){
-		chart.data.datasets[0].data.push(parseInt(document.getElementsByName(inputList[i])[0].value));
+
+	// Horizontal bar chart
+	horizontalBarChart_A = new Chart(document.getElementById("bar-chart-horizontal_A"), {
+		type: 'horizontalBar',
+		data: {
+		  labels: [],
+		  datasets: [
+			{
+			  label: "",
+			  backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9"],
+			  data: []
+			}
+		  ]
+		},
+		options: {
+		  legend: { display: false },
+		  title: {
+			display: false,
+			text: ''
+		  },
+		  scales: {
+			  xAxes: [{
+				  ticks: {
+						  beginAtZero: true
+					  }
+			  }]
+		  }
+		}
+	});
+	horizontalBarChart_B = new Chart(document.getElementById("bar-chart-horizontal_B"), {
+		type: 'horizontalBar',
+		data: {
+		  labels: [],
+		  datasets: [
+			{
+			  label: "",
+			  backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9"],
+			  data: []
+			}
+		  ]
+		},
+		options: {
+		  legend: { display: false },
+		  title: {
+			display: false,
+			text: ''
+		  },
+		  scales: {
+			  xAxes: [{
+				  ticks: {
+						  beginAtZero: true
+					  }
+			  }]
+		  }
+		}
+	});
+	for ( i=0; i< products.length; i++){
+		barChart.data.labels[i] = products[i];
+		barChart.data.datasets[1].data[i] = competitorDataList[i];
+		horizontalBarChart_A.data.labels[i] = products[i];
+		horizontalBarChart_B.data.labels[i] = '';
+		horizontalBarChart_B.data.datasets[0].data[i] = competitorDataList[i];
 	}
-	chart.update();
-	
-	
-	
+	console.log("lol", horizontalBarChart_B.data);
+	barChart.update();
+	horizontalBarChart_A.update();
+	horizontalBarChart_B.update();
 }
 
+function updateGraph(){
+	//Bar Graph
+	console.log(barChart.data);
+
+	for ( i=0; i< inputList.length; i++){
+		barChart.data.datasets[0].data[i] = parseInt(document.getElementsByName(inputList[i])[0].value);
+		horizontalBarChart_A.data.datasets[0].data[i] = parseInt(document.getElementsByName(inputList[i])[0].value);
+	}
+	barChart.update();
+	horizontalBarChart_A.update();
+	horizontalBarChart_B.update();
+}
